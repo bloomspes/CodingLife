394483321831506c53ad374eb9589763de5170b2
Spes <bloomspes@gmail.com>
2021-12-22 17:39:31 +0900

    add: 컨트롤러 테스트코드 작성

--- 20211220/app/src/test/java/TDD/ReviewControllerTest.java
+++ 20211220/app/src/test/java/TDD/ReviewControllerTest.java
@@ -1,2 +1,48 @@
-package TDD;public class ReviewControllerTest {
+package TDD;
+
+import TDD.application.ReviewService;
+import TDD.domain.Review;
+import TDD.ui.ReviewController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ReviewController.class)
+public class ReviewControllerTest {
+
+    private MockMvc mockMvc;
+
+    public ReviewControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @MockBean
+    private ReviewService reviewService;
+
+    private Long id = 1L;
+    private String content = "training";
+    private String phone = "010-1111-2222";
+
+    @Test
+    void 리뷰는_id가_존재할때_id와_200Status_를_리턴한다() throws Exception {
+        // 준비
+        given(reviewService.getById()).willReturn(new Review(id, content, phone));
+
+        // 실행
+        ResultActions perform = mockMvc.perform(get("/reviews/" + id));
+
+        // 검증
+        perform
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(id))
+                .andExpect(jsonPath("content").value(content))
+                .andExpect(jsonPath("phone").value(phone));
+    }
 }
