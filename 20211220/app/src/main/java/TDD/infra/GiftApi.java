d8b721507b39426dbd9ed1baa7d0446555274774
Spes <bloomspes@gmail.com>
2021-12-22 17:44:04 +0900

    add: 선물하기 API 코드를 요청에 따른 비동기식으로 수정하라.

--- 20211220/app/src/main/java/TDD/infra/GiftApi.java
+++ 20211220/app/src/main/java/TDD/infra/GiftApi.java
@@ -1,4 +1,27 @@
-package TDD;
+package TDD.infra;
 
+import TDD.domain.Review;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
 public class GiftApi {
+
+   private final WebClient client;
+   private Review r;
+
+   public GiftApi(WebClient.Builder builder) {
+      this.client = builder.baseUrl("http://youthcon.seok2.dev/apis/v1/send").build();
+   }
+
+   private final Integer AMOUNT = 1000;
+
+   public Boolean send(String phone) {
+      return this.client.post()
+              .uri("/reviews/" + r.getPhone())
+              .body(Mono.just(r), Review.class)
+              .retrieve()
+              .bodyToMono(Review.class);
+   }
 }
